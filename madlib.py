a0e1523490b78701bcaf04f0ea3e106bbc0a9ad3
Jonathan Tucker <71109818+jontucklogic@users.noreply.github.com>
2021-06-27 08:02:11 -0500

    Initial commit

---
+++ madlib.py
@@ -0,0 +1,17 @@
+# I have a(n) [adjective] idea for a program! It will watch [plural noun] and count how many times they [verb]. I don’t think it will [verb] the world or anything, but I’m [adjective] about it. My [family member] said they are going to build a program to [verb] [plural noun]. Which idea is [adjective]?
+
+adjective1 = input('Adjective: ')
+plural_noun2 = input('Plural Noun: ')
+verb3 = input('Verb: ')
+verb4 = input('Verb: ')
+adjective5 = input('Adjective: ')
+family_member6 = input('Family Member: ')
+verb7 = input('Verb: ')
+plural_noun8 = input('Plural Noun: ')
+adjective9 = input('Adjective: ')
+
+print(f'I have a(n) {adjective1} idea for a program!')
+print(f'It will watch {plural_noun2} and count how many times they {verb3}.')
+print(f'I don’t think it will {verb4} the world or anything, but I’m {adjective5} about it.')
+print(f'My {family_member6} said they are going to build a program to {verb7} {plural_noun8}.')
+print(f'Which idea is {adjective9}')
---
+++ madlib2.py
@@ -0,0 +1,24 @@
+# I like to have [plural noun], [plural noun], and [plural noun] on my desk at all times. This helps me stay focused when I [verb]. I also keep my [noun] closed to keep [nouns] from bothering me while I [verb]. When I’m [verb -ing], I use the [name] Technique. It’s where you spend [amount of time] [verb -ing] and then [amount of time] [verb -ing]. This makes me a(n) [adjective] worker.
+
+
+plural_noun1 = input('Plural Noun: ')
+plural_noun2 = input('Plural Noun: ')
+plural_noun3 = input('Plural Noun: ')
+verb4 = input('Verb: ')
+noun5 = input('Noun: ')
+plural_noun6 = input('Plural Noun: ')
+verb7 = input('Verb: ')
+verb8 = input('Verb -ing: ')
+name9 = input('Name: ')
+amount_of_time10 = input('Amount of Time: ')
+verb11 = input('Verb -ing: ')
+amount_of_time12 = input('Amount of Time: ')
+verb13 = input('Verb -ing: ')
+adjective14 = input('Adjective: ')
+
+print(f'I like to have {plural_noun1}, {plural_noun2}, and {plural_noun3} on my desk at all times.')
+print(f'This helps me stay focused when I {verb4}.')
+print(f'I also keep my {noun5} closed to keep {plural_noun6} from bothering me while I {verb7}.')
+print(f'When I’m {verb8}, I use the {name9} Technique.')
+print(f'It’s where you spend {amount_of_time10} {verb11} and then {amount_of_time12} {verb13}.')
+print(f'This makes me a(n) {adjective14} worker.')
